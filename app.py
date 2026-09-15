e4942138896f9d550485c4400821aaf50dc209bc
imranjk02 <imrankotwal02@gmail.com>
2026-09-15 12:59:10 +0530

    Fix MySQL environment variables

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app.secret_key = "nettech_secret_key"
 
 def get_db():
     return mysql.connector.connect(
-        host=os.environ.get("MYSQL_PUBLIC_HOST"),
-        port=int(os.environ.get("MYSQL_PUBLIC_PORT")),
+        host=os.environ.get("MYSQLHOST"),
+        port=int(os.environ.get("MYSQLPORT", 3306)),
         user=os.environ.get("MYSQLUSER"),
         password=os.environ.get("MYSQLPASSWORD"),
         database=os.environ.get("MYSQLDATABASE")
